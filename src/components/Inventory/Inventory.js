2290b647a08206cf3218e3dd2ad3f259cb9fe775
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-01 10:46:07 +0600

    created the routing framework

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>This is in inventory</h1>
+        </div>
+    );
+};
+
+export default Inventory;
