f607d512b03af5d21c4e98c4740dd7af984982df
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-03 15:57:49 +0600

    added eventhandler to delete button and made it functional

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -7,11 +7,16 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 const Orders = () => {
     const [products, handleProducts] = useProducts();
     const [cart, handleCart] = useCart(products);
+    const removeFromCart = (product) => {
+        // Here, we are selecting the rest of the items and setting those items to state
+        const restItems = cart.filter(pd => pd.id !== product.id)
+        handleCart(restItems)
+    }
     return (
         <div className='shop-container'>
             <div className="review-items-container">
                 {
-                    cart.map(product => <ReviewItem key={product.id} product={product}></ReviewItem>)
+                    cart.map(product => <ReviewItem key={product.id} product={product} removeFromCart={removeFromCart}></ReviewItem>)
                 }
             </div>
             <div className="cart-container">
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,8 +1,10 @@
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon, faTrashAlt } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItem.css'
 
-const ReviewItem = (props) => {
-    const { name, img, price, shipping, quantity } = props.product
+const ReviewItem = ({ product, removeFromCart }) => {
+    const { name, img, price, shipping, quantity } = product
     return (
         <div className='review-item'>
             <div>
@@ -18,7 +20,9 @@ const ReviewItem = (props) => {
                     <p><small>Quantity: {quantity}</small></p>
                 </div>
                 <div className="delete-container">
-                    <button>Delete</button>
+                    <button onClick={() => removeFromCart(product)} className='delete-btn'>
+                        <FontAwesomeIcon className='trash-icon' icon={faTrashCan}></FontAwesomeIcon>
+                    </button>
                 </div>
             </div>
         </div>
