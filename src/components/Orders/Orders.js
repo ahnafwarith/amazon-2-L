f675cca41f393e33df888b398d3c37f9a0b4ee61
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-03 16:40:48 +0600

    received children props and showed it dynamically in cart

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/UseCart';
 import useProducts from '../../hooks/UseProducts';
 import { removeFromDb } from '../../utilities/fakedb';
@@ -23,7 +24,11 @@ const Orders = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <Link to='/inventory'>
+                        <button>Proceed to inventory</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,6 +4,7 @@ import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 import useProducts from '../../hooks/UseProducts'
+import { Link } from 'react-router-dom';
 
 const Shop = () => {
     const [products, handleProducts] = useProducts()
@@ -53,7 +54,11 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart cart={cart}>
+                    <Link to='/orders'>
+                        <button>Review Orders</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
     );
